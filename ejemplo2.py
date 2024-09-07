09421a95cf600ffaaf0e9b5eee0525a94b2bd337
Nelson Ramirez <xhyskoax@gmail.com>
2024-09-06 22:10:11 -0400

    primera versión con función de lectura y escritura en csv

--- ejemplo2.py
+++ ejemplo2.py
@@ -2,12 +2,13 @@ import uuid
 import os
 import platform
 import time
+import csv
 
 delay = 2
 
 class Animal:
-    def __init__(self, nombre, raza, especie):
-        self.id  = str(uuid.uuid4())[0:8]
+    def __init__(self, nombre, raza, especie, id = str(uuid.uuid4())[0:8]):
+        self.id = id
         self.nombre = nombre
         self.raza = raza
         self.especie = especie
@@ -18,11 +19,35 @@ class Animal:
 
 def leer_animales():
     print("Se está leyendo el archivo animales.csv")
-    time.sleep(delay)
+    with open("./archivos/animales.csv", "r", newline='') as archivo_animales:
+        data = csv.reader(archivo_animales)
+        data = list(data)
+        
+        print("****************INICIO DATOS****************")
+        for index, fila in enumerate(data[1:]):
+            al = fila[0].split(";")
+            # order de parámetros (nombre, raza, especie, id)
+            animal = Animal(al[1], al[2], al[3], al[0])
+            print(f"Indice: {index} -> {animal}")
+
+            
+        print("****************FIN DATOS****************\n\n")
+
 
 def registrar_animal():
-    print("Se procede a registrar un animal.")
-    time.sleep(delay)
+    print("Ingrese los datos del nuevo animal:")
+    
+    nombre = input("Nombre del animal: ")
+    raza = input("Raza del animal: ")
+    especie = input("Especie del animal: ")
+    
+    nuevo_animal = Animal(nombre, raza, especie)
+    
+    with open("./archivos/animales.csv", "a", newline="\n") as archivo_animales:
+        writer = csv.writer(archivo_animales, delimiter=";")
+        lista_valores = [nuevo_animal.id, nuevo_animal.nombre, nuevo_animal.raza, nuevo_animal.especie]
+        writer.writerow(lista_valores)
+        print(f"Anima {nuevo_animal.nombre} registrado con éxito.")
     
 def salir():
     print("Gracias por utilizar el sistema.")
@@ -54,10 +79,8 @@ def menu():
     
     if opcion == "1":
         leer_animales()
-        limpiar_consola()
     elif opcion == "2":
         registrar_animal()
-        limpiar_consola()
     else: 
         print("Opción inválida.")
         time.sleep(delay)
